d59475591e17a8b38e81469f32d8e7b0b05fd7ed
阿豪boy <504595380@qq.com>
2022-04-01 11:47:05 +0800

    feat: del log

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,3 @@
 export * from "./react";
 export * from "./vanilla";
 export * from "./derive";
-console.log("-------", __DEV__);
-
-if (__DEV__) {
-  console.log("333333333333");
-}else{
-  console.log("4444444444");
-}
